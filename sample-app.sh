40078c0c93309f2872d2eb7a383d74b2435b0b40
eet0031 <84158121+eet0031@users.noreply.github.com>
2022-11-27 15:09:56 +1300

    Update sample-app.sh

--- sample-app.sh
+++ sample-app.sh
@@ -10,8 +10,8 @@ echo "RUN pip install flask" >> tempdir/Dockerfile
 echo "COPY  assessment_code.py /home/myapp/" >> tempdir/Dockerfile
 echo "COPY  network_equipment.json /home/myapp/" >> tempdir/Dockerfile
 echo "EXPOSE 5050" >> tempdir/Dockerfile
-echo "CMD python /home/myapp/assessment_code.py &" >> tempdir/Dockerfile
+echo "CMD python /home/myapp/assessment_code.py" >> tempdir/Dockerfile
 
 cd tempdir
 docker build -t assessmentcodeimage .
-docker run -p 5050:5050 --name assessment2container assessmentcodeimage &
+docker run -p 5050:5050 --name assessment2container assessmentcodeimage
